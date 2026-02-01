a0aab068bcd1bba3f5a89d751dc4928c3563fd81
Ganesh Duggirala <ganeshdugirala@gmail.com>
2026-02-01 19:23:10 +0530

    old code branch update

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function LandingPage() {
       threshold: 0.1,
       rootMargin: '0px 0px -50px 0px'
     };
+    
 
     const observer = new window.IntersectionObserver((entries) => {
       entries.forEach(entry => {
